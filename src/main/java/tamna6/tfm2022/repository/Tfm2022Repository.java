d0b5da4c33c18732b81330e925514f84cf227c2e
SeDevel <31820402+SeDevel@users.noreply.github.com>
2022-09-07 18:04:03 +0900

    Revert "로그인 구현"
    This reverts commit 091c822c4985fa2bf6bb9f99ef6e4d300d8e2b70.

--- src/main/java/tamna6/tfm2022/repository/Tfm2022Repository.java
+++
@@ -1,7 +0,0 @@
-package tamna6.tfm2022.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import tamna6.tfm2022.entity.TfmUser;
-
-public interface Tfm2022Repository extends CrudRepository<TfmUser, String> {
-}
